b765bbaedec71b90a5dc0fa6b6fc0c9601b1da9b
Liridon Sejdiu <liridonsejdiu88@gmail.com>
2020-04-29 23:22:36 +0200

    image card component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import ImagesCard from './components/ImagesCard'
 
 function App() {
   // app level stat
@@ -11,42 +12,22 @@ function App() {
       `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl">Photo by LiRA</div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            400
-          </li>
-          <li>
-            <strong>Likes:</strong>
-            356
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-          #tag1
-        </span>
+    <div className="container mx-auto">
+      <div className="grid-cols-3 grid gap-4">
+        {images.map(image =>(
+          <ImagesCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
+
   );
 }
 
