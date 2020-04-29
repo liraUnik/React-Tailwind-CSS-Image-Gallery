c5739030413875918edd51ce9fe36a2e514a0e41
Liridon Sejdiu <liridonsejdiu88@gmail.com>
2020-04-29 22:41:25 +0200

    env file,usestate and useeffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  // app level stat
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
@@ -22,9 +27,15 @@ function App() {
         </ul>
       </div>
       <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag1</span>
-        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag1</span>
-        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag1</span>
+        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          #tag1
+        </span>
+        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+          #tag1
+        </span>
       </div>
     </div>
   );
