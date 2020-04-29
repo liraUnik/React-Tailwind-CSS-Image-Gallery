cbfb151b59c2b1e641f7af33fee8525ed95538e7
Liridon Sejdiu <liridonsejdiu88@gmail.com>
2020-04-29 22:01:15 +0200

    clean up and simple card app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl">Photo by LiRA</div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            400
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            356
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag1</span>
+        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag1</span>
+        <span className="inline-block bg-gray-200 mr-2 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#tag1</span>
+      </div>
     </div>
   );
 }
